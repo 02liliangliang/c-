5e2a5dcd3a0fa0e46a9d7d81c26a810888d04171
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-15 16:20:05 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include<stdio.h>
 int main()
-   {float a;
-    a=10000/3.0;
-    printf("%f\n",a);
+{
+    int max(int x,int y);
+    int a,b,c;
+    scanf("%d,%d",&a,&b);
+    c=max(a,b);
+    printf("max=%d\n",c);
+    return 0;
+}
+int max(int x,int y)
+{
+    int z;
+    if(x>y)z=x;
+    else z=y;
     return 0;
 }
