09e759ae49afe8a0a41febdb031dee3480f777a8
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-20 17:35:11 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
 #include<stdio.h>
-
 int main()
 {
-    float h, r, l, s, sq, vq, vz;
-    float pi = 3.141526;
-    printf("请输入圆半径r，圆柱高h∶");
-    scanf("%f %f", &r, &h);
-    l = 2 * pi*r;
-    s = r*r*pi;
-    sq = 4 * pi*r*r;
-    vq = 3.0 / 4.0*pi*r*r*r;
-    vz = pi*r*r*h;
-    printf("圆周长为:       l=%6.2f\n", l);
-    printf("圆面积为:       s=%6.2f\n", s);
-    printf("圆球表面积为:   sq=%6.2f\n", sq);
-    printf("圆球体积为:     v=%6.2f\n", vq);
-    printf("圆柱体积为:     vz=%6.2f\n", vz);
+    int c1, c2;
+    printf("请输入两个字符c1,c2:");
+    c1 = getchar();
+    c2 = getchar();
+    printf("用putchar语句输出结果为:");
+    putchar(c1);
+    putchar(c2);
+    printf("\n");
+    printf("用printf语句输出结果为:");
+    printf("%c %c\n", c1, c2);
     return 0;
 }
 
