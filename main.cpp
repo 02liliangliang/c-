861cd94aa5dbad3cf8fe031ed58f3bdf789eead1
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-20 17:35:37 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int c1, c2;
-    printf("请输入两个字符c1,c2:");
-    c1 = getchar();
-    c2 = getchar();
-    printf("用putchar语句输出结果为:");
-    putchar(c1);
-    putchar(c2);
-    printf("\n");
-    printf("用printf语句输出结果为:");
-    printf("%c %c\n", c1, c2);
+    int a = 3, b = 4, c = 5;
+    printf("%d\n", a || b + c && b - c);
     return 0;
 }
-
