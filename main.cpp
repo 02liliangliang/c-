97a9c0359b0a8bb774aa2bb551131b8bb7dec6de
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-20 17:42:21 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include<stdio.h>
-int main(){
-    int x,y;
-    printf("请输入x的值：");
-    scanf("%d",&x);
-    if(x<1){
-        y=x;
-    }else if(x>1&&x<10){
-        y=2*x-1;
-    }else{
-        y=3*x-11;
+int main()
+{
+    int score;
+    printf("输入学生的成绩:\n");
+    scanf("%d",&score);
+ if (score<0 || score>100){
+    printf("输入的数据非法!\n");
+}else{
+    if(score>90){
+        printf("该同学的等级为:A");
     }
-    printf("x=%d,y=%d",x,y);
+    if(score>=80 && score<=90){
+        printf("该同学的等级为:B");
+    }
+    if(score>=70 && score<=79){
+        printf("该同学的等级为:C");
+    }
+    if(score>=60 && score<=69){
+        printf("该同学的等级为:D");
+    }
+    if(score>=0&&score<=59){
+        printf("该同学的等级为:E");
+    }
+    }
     return 0;
 }
