e93dd0ccfe34d93b664c3ca90781788692fad101
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-20 17:41:41 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
-#include <stdio.h>
-int main()
-{
-    int x, y;
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("请输入x的值：");
     scanf("%d",&x);
-    if (x < 1) {
-        y = x;
+    if(x<1){
+        y=x;
+    }else if(x>1&&x<10){
+        y=2*x-1;
+    }else{
+        y=3*x-11;
     }
-    else if (x >= 1 && x < 10) {
-        y = 2 * x - 1;
-    }
-    else {
-        y = 3 * x - 11;
-    }
-    printf("y = %d\n", y);
-
+    printf("x=%d,y=%d",x,y);
     return 0;
 }
-
