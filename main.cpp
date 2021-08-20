f3eeb72e7e7befb0de08424c1592522faf4757fc
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-20 17:40:53 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-#include<stdio.h>
-int main(){
-    int x,y;
-    printf("请输入x的值：");
-    scanf("%d",&x);
-    if(x<1){
-        y=x;
-    }else if(x>1&&x<10){
-        y=2*x-1;
-    }else{
-        y=3*x-11;
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float a, b;
+    scanf("%f", &a);
+    if (a >= 1000 || a < 0) {
+        printf("请输入小于1000的正数\n");
+        scanf("%f", &a);
+        b = sqrt(a);
     }
-    printf("x=%d,y=%d",x,y);
+    else {
+        b = sqrt(a);
+    }
+    printf("a=%4.0f, b=%4.0f\n", a, b);
     return 0;
 }
