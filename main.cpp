5208f3157e17edce681b551285b427df63de6f35
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-20 17:49:54 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,21 +2,16 @@
 
 int main()
 {
-        double total_sum = 0, sum1 = 0, sum2 = 0, sum3 = 0.0;
-        for (int k = 1; k <= 100; k++)
+    int a, b, c;
+    for (int i = 100; i <= 999; i++)
+    {
+        a = i / 100;
+        b = (i / 10) % 10;
+        c = i % 10;
+        if (a * a * a + b * b * b + c * c * c == i)
         {
-                sum1 += k;
-                if (k <= 50)
-                {
-                        sum2 += k * k;
-                }
-                //遍历10次就不在执行情况3
-                if (k <= 10)
-                {
-                        sum3 += 1.0 / k;
-                }
+            printf("%d\n", i);
         }
-        total_sum = sum1 + sum2 + sum3;
-        printf("三种情况求和结果为：%lf\n", total_sum);
-        return 0;
+    }
+    return 0;
 }
