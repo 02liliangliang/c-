5d518275613424db59aabdade1c68c1658b4a805
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-20 17:49:37 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-        double total_sum = 0;
-        for(int i = 1; i <= 20; i++)
+        double total_sum = 0, sum1 = 0, sum2 = 0, sum3 = 0.0;
+        for (int k = 1; k <= 100; k++)
         {
-                double single_sum = 1;
-                for (int j = i; j > 0; j--)
+                sum1 += k;
+                if (k <= 50)
                 {
-                        single_sum *= j;
+                        sum2 += k * k;
                 }
-                total_sum += single_sum;
+                //遍历10次就不在执行情况3
+                if (k <= 10)
+                {
+                        sum3 += 1.0 / k;
+                }
         }
-        printf("1~20每个数字阶乘总和为：%lf\n",total_sum);
+        total_sum = sum1 + sum2 + sum3;
+        printf("三种情况求和结果为：%lf\n", total_sum);
         return 0;
 }
