e67ca26c70b541770fb28589fc5df3c3ced51f1a
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-20 17:45:30 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
-#include <stdio.h>
-#include<math.h>
+# include <stdio.h>
+# include <math.h>
+
 int main()
-{ int h=10;
-float x1=2,y1=2,x2=-2,y2=2,x3=-2,y3=-2,x4=-2,y4=-2,x,y,d1,d2,d3,d4;
-printf("请输入一个点(x,y)：");
-scanf("%f,%f",&x,&y);
-d1=sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1));
-d2=sqrt((x-x2)*(x-x2)+(y+y2)*(y+y2));
-d3=sqrt((x+x3)*(x+x3)+(y-y3)*(y-y3));
-d4=sqrt((x+x4)*(x-x4)*(y+y4)*(y+y4));
-if(d1>1&&d2>1&&d3>1&&d4>1) h=0;
-printf("该点高度为%d\n",h);
+{
+    int sign = 1;
+    double pi = 0.0, term = 1.0;
+    int n = 0;
+
+    while (fabs(term) >= 1e-6)
+    {
+        n++;
+        term = 1.0 / (2 * n - 1)*sign;
+        pi += term;
+        sign = -sign;
+    }
+    pi *= 4;
+    printf("pi的近似值是%lf\n", pi);
+    printf("循环体循环了%d次\n", n);
+    return 0;
 }
+
