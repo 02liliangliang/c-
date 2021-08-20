862d840565fbb1a1ff013fa9f3bc64e0605981cf
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-20 17:37:17 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 int main()
 {
     int a = 3, b = 4, c = 5;
-    printf("%d\n", !(a > b)&& !c|| 1);
+    printf("%d\n", !(a + b) + c - 1);
+    printf("%d\n", !(a + b) + c - 1 && b + c / 2);
     return 0;
 }
-
