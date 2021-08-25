1490932a41f550567ade521affe1b491608095c8
liliangliang <88825551+02liliangliang@users.noreply.github.com>
2021-08-25 23:11:27 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,67 +1,47 @@
-#include <stdio.h>
+#include<stdio.h>
+#define M 3
+#define N 4
+
 int main()
 {
-        int a[15][15], n, i, j, k;
-        while (1)
+        int max, min, rowindex, colindex, flag = 0;
+        int array[M][N];
+        printf("请输入%d行%d列的数组:\n", M, N);
+        for (int i = 0; i < M; i++)
         {
-                printf("请输入n(1~15):");
-                scanf("%d", &n);
-                if (n != 0 && n <= 15 && n % 2 != 0)
+                for (int j = 0; j < N; j++)
+                        scanf("%d", &array[i][j]);
+        }
+
+        for (int i = 0; i < M; ++i)
+        {
+                max = array[i][0];
+                for (int j = 0; j < N; ++j)
+                {
+                        if (array[i][j] > max)
+                        {
+                                max = array[i][j];
+                                colindex = j;
+                        }
+                }
+                min = array[0][colindex];
+                for (int j = 0; j < M; ++j)
+                {
+                        if (array[j][colindex] < min)
+                        {
+                                min = array[j][colindex];
+                                rowindex = j;
+                        }
+                }
+                if (max == min && i == rowindex)
+                {
+                        flag = 1;
+                        printf("鞍点为：%d行%d列的元素%d", rowindex, colindex, max);
                         break;
-                else
-                {
-                        printf("请输入奇数\n");
                 }
         }
 
-        for (i = 1; i <= n; i++)
-        {
-                for (j = 1; j <= n; j++)
-                        a[i][j] = 0;
-        }
-
-        j = n / 2 + 1;
-        a[1][j] = 1;
-        i = 1;
-        for (k = 2; k <= n*n; k++)
-        {
-                i -= 1;
-                j += 1;
-
-                if (i<1 && j>n)
-                {
-                        i += 2;
-                        j -= 1;
-                }
-                else if (i<1)
-                {
-                        i = n;
-                }
-                else if (j>n)
-                {
-                        j = 1;
-                }
-
-                if (a[i][j] == 0)
-                {
-                        a[i][j] = k;
-                }
-                else
-                {
-                        i += 2;
-                        j -= 1;
-
-                        a[i][j] = k;
-                }
-        }
-
-        for (i = 1; i <= n; i++)
-        {
-                for (j = 1; j <= n; j++)
-                        printf("%5d", a[i][j]);
-
-                printf("\n");
-        }
-
+        if (0 == flag)
+                printf("没有鞍点");
         return 0;
 }
